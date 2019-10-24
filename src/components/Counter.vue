9c3d9c5728429036ac07b3831b14042de0fd51f5
mojp <1260350028@qq.com>
2019-10-24 10:50:41 +0800

    父子组件传值

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="increment">+</button>
+        <button @click="decreament">-</button>
+        <p><span>{{num}}</span></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"conter",
+        props:['num'],
+        data(){
+            return {
+              
+            }
+        },
+        methods:{
+            increment(){
+                this.$emit("incre");
+            },
+            decreament(){
+               this.$emit("decre");
+            }
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <h1>{{msg}}</h1>
+        <counter v-bind:num="num" v-on:incre="increment" v-on:decre="decrement"></counter>
+        <span>parent : {{num}}</span>
+    </div>
+</template>
+
+<script>
+    import Counter from './Counter'
+    export default {
+        name:"hello",
+        data(){
+            return{
+                num:10,
+                msg:"Welcome to Vue,"
+            }
+        },
+        components:{
+            Counter
+        }, 
+        methods:{
+            increment(){
+this.num++;
+            },
+            decrement(){
+                this.num--;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ h1{
+     font-weight: normal;
+ }
+
+</style>
